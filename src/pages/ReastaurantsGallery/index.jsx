66d20d492ddc272ba4c4815aebd7becda6406b09
Tudor Tocan <tudor.tocan@gmail.com>
2024-02-16 19:33:03 +0000

    remove default styles, add search button and axios request

--- src/pages/ReastaurantsGallery/index.jsx
+++ src/pages/ReastaurantsGallery/index.jsx
@@ -1,5 +1,22 @@
+import axios from "axios";
+
 const RestaurantsGallery = () => {
-  return <div>Restaurants Gallery</div>;
+  const getRestaurantsData = () => {
+    axios
+      .get(
+        `https://random-data-api.com/api/restaurant/random_restaurant?size=6`
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
+  return (
+    <div>
+      <div>Search For Restaurants</div>
+      <button onClick={getRestaurantsData}>Search</button>
+    </div>
+  );
 };
 
 export default RestaurantsGallery;
