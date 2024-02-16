3f0ce3ed507ca2ca8fca7827c60140d2905a618e
Tudor Tocan <tudor.tocan@gmail.com>
2024-02-16 20:05:49 +0000

    add logic to conditionally render search button/ restaurant cards set data to state

--- src/pages/ReastaurantsGallery/index.jsx
+++ src/pages/ReastaurantsGallery/index.jsx
@@ -1,20 +1,54 @@
 import axios from "axios";
+import { useState } from "react";
+import RestCard from "../../components/RestCard";
 
 const RestaurantsGallery = () => {
+  const [responseData, setResponseData] = useState({
+    isDataAvailable: false,
+    results: [],
+  });
+
   const getRestaurantsData = () => {
+    // axios request to get random data api restaurants
     axios
       .get(
         `https://random-data-api.com/api/restaurant/random_restaurant?size=6`
       )
       .then((res) => {
-        console.log(res.data);
+        // setting the data in the state
+        // isDataAvailable relies on the res.status being successfull. if the status is anything else, the search button will still be displayed
+        setResponseData({
+          isDataAvailable: res.status === 200,
+          results: res.data,
+        });
       });
   };
 
   return (
     <div>
       <div>Search For Restaurants</div>
-      <button onClick={getRestaurantsData}>Search</button>
+      {/* conditionally rendering the search button */}
+      {!responseData.isDataAvailable && (
+        <button onClick={getRestaurantsData}>Search</button>
+      )}
+
+      {/* conditionally rendering the Restaurant Cards */}
+      {responseData.isDataAvailable && (
+        <div>
+          {responseData.results.map((restaurant) => {
+            return (
+              <>
+                <RestCard
+                  key={restaurant.id}
+                  restName={restaurant.name}
+                  restPhoneNo={restaurant.phone_number}
+                  restAddress={restaurant.address}
+                />
+              </>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
