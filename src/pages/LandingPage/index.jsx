f064d875e1093c03c46a5ffe5c493d4f6dd93248
Tudor Tocan <tudor.tocan@gmail.com>
2024-02-16 19:00:02 +0000

    add landing page

---
+++ src/pages/LandingPage/index.jsx
@@ -0,0 +1,13 @@
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <div>
+        Welcome to the restaurants app. Please go to the restaurants page to get
+        a random selection of restaurants{" "}
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
